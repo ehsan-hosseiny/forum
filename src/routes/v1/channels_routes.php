df0042b66ce489c08f9fe6c0c065f8f5852e2bca
pc <ehsanhossini@gmail.com>
2022-01-17 19:03:14 +0330

    modification on route middleware

--- src/routes/v1/channels_routes.php
+++ src/routes/v1/channels_routes.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\api\v1\channel\ChannelController;
 
 Route::prefix('/channel')->group(function (){
     Route::get('/all', [ChannelController::class, 'getAllChannelsList'])->name('channel.all');
-    Route::middleware('can:channel management')->group(function (){
+    Route::middleware(['channel management'=>'auth:sanctum'])->group(function (){
         Route::post('/create', [ChannelController::class, 'createNewChannel'])->name('channel.create');
         Route::put('/update', [ChannelController::class, 'updateChannel'])->name('channel.update');
         Route::delete('/delete', [ChannelController::class, 'deleteChannel'])->name('channel.delete');
